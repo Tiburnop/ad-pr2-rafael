00a521c3f463d71eeb1d1b7e8065588c924b9c32
Tiburnop <rafael.galvan.pomares@gmail.com>
2024-12-04 13:37:38 +0100

    actualizo

---
+++ backend2/routes/api.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HelloWorldController;
+use App\Http\Controllers\CsvController;
+
+
+Route::apiResource('hello', HelloWorldController::class);
+
+
+use App\Http\Controllers\JsonController;
+
+// Rutas para JSON
+Route::get('/json', [JsonController::class, 'index']);
+Route::post('/json', [JsonController::class, 'store']);
+Route::get('/json/{filename}', [JsonController::class, 'show']);
+Route::put('/json/{filename}', [JsonController::class, 'update']);
+Route::delete('/json/{filename}', [JsonController::class, 'destroy']);
+//Rutas para CSV
+Route::get('/csv', [CsvController::class, 'index']);
+Route::get('/csv/{id}', [CsvController::class, 'show']);
+Route::post('/csv', [CsvController::class, 'store']);
+Route::put('/csv/{id}', [CsvController::class, 'update']);
+Route::delete('/csv/{id}', [CsvController::class, 'destroy']);
+
+
+
